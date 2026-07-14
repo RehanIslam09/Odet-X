124e926923a6326cc43a4d970c1de8a1f87695f3
Rehan Islam <rehanislamcontact@gmail.com>
2026-07-14 02:28:05 +0000

    chore: initialize frontend project architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div>App</div>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
 export default App;
